c4c434880c450bfe003acf6728f8aa4b95e99301
Bryce Slade <bryceslade@gmail.com>
2021-10-08 12:33:13 -0600

    Finshed layout, on to styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+     .then(response => {
+       setCharacters(response.data);
+
+     }).catch(error => {
+       console.error(error);
+     })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        characters.map(items => {
+          return <Character key = {items.id} info = {items}/>
+        })
+      }
     </div>
   );
 }
 
 export default App;
-// hi
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterStyle = styled.div `
+    padding: 2rem;
+    font-family: 'Gemunu Libre', sans-serif;
+`;
+
+const InfoStyle = styled.div`
+    border: 2px dashed black;
+    width: 20%;
+    margin-left: 40%;
+    font-size: 1.5rem;
+`;
+
+const NameStyle = styled.h2`
+    font-size: 3rem;
+`;
+
+export default function Character({info}) {
+
+    return (
+        <CharacterStyle>
+            <NameStyle>{info.name}</NameStyle>
+            <InfoStyle>
+                <h3>Gender: {info.gender}</h3>
+                <h3>Height: {info.height}</h3>
+                <h3>Mass: {info.mass}</h3>
+                <h3>Birth Year: {info.birth_year}</h3>
+                <h3>Eye Color: {info.eye_color}</h3>
+                <h3>Hair Color: {info.hair_color}</h3>
+                <h3>Skin Color: {info.skin_color}</h3>
+            </InfoStyle>
+        </CharacterStyle>
+    )
+}
